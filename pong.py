a1e724e5463cb8699423d18d18326f751f1d643a
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2019-04-30 23:38:40 +0100

    base

--- pong.py
+++ pong.py
@@ -10,14 +10,16 @@ import time
 
 pixels = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
 
+direction_of_travel = "ne"
+
 def print_screen():
-  print("--Pong--")
+  print("----------Pong-----------")
   #print(grid_height)
   for pixel_row in pixels:
     for pixel in pixel_row:
       #s += elem
-      print(pixel, end = '')
-    print("")
+      print(" ", pixel, end = '')
+    print(" ")
 
 print_screen()
 
@@ -36,8 +38,17 @@ print("hello2")
 loops = 0
 while loops < 10:
     #print(loops)
+    pixels[1][1] = loops
+
+    #for pixel_row in pixels:
+    #  for pixel in pixel_row:
+    #    pixel = 3
+
     os.system("clear")
     print_screen()
-    pixels[1][1] = loops
+    
+    # increment the loop variable
     loops += 1
+
+    # have a wee sleep
     time.sleep(0.2)
